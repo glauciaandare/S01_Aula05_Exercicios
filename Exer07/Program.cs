efbe909a42c6f32498063e876d4fe8c62f79b417
Glaucia Andare <glaucia.andare@gmail.com>
2022-09-25 20:19:02 -0300

    Add project files.

---
+++ Exer07/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * 7) Num programa que lê um valor inteiro, faça a sua validação através do tratamento de uma exceção. Caso não
+ *  seja inteiro continuar pedindo a introdução do valor inteiro. 
+ */
+
+namespace Exer07 {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Informe um número Inteiro: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                while (numero < 0)
+                {
+
+                    
+                }
+            }
+            
+            
+
+            catch 
+            {
+                Console.WriteLine("O número dever ser inteiro");
+            }
+        }
+    }
+}
